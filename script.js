e76b717027c3004df5bc4bf399eb8fa8e69a041f
Kyamu <124717126+Kyamu@users.noreply.github.com>
2023-04-20 23:45:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+// JavaScript code
+const evaluatedText = document.getElementById("evaluatedText");
+const letterCount = document.getElementById("letterCount");
+
+evaluatedText.addEventListener("input", () => {
+  letterCount.textContent = evaluatedText.value.length;
+});
+
